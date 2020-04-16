21d3389351be477b893d8eb92e17c985f48a4288
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-16 11:20:47 -0700

    first commit

---
+++ greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+let user = [1, 2, 3];
+// let user = "Jane User";
+
+console.log(greeter());
+// document.body.textContent = greeter(user);
